c4824ea721d66a0fb84fd7a0bb6be893f190e621
satyam4484 <satyambsingh93@gmail.com>
2025-11-19 18:43:20 +0530

    basic crud operation

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Task;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {}
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    public Task findByTitle(String title);
+}
